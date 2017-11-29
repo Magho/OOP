281e95b86cf137b4b545849f130f0dc97c72e131
MohamedMashaal <mohamedmashaal1997@gmail.com>
2017-11-29 17:32:13 +0200

    Use this shit

--- cs_221/src/eg/edu/alexu/csd/oop/db/cs55/creationDatbaseHndler.java
+++ cs_221/src/eg/edu/alexu/csd/oop/db/cs55/creationDatbaseHndler.java
@@ -11,12 +11,41 @@ public class creationDatbaseHndler {
 	}
 	
 	public void createDatabse (String databasename){
-		File database =  new File (file.getAbsolutePath() +file.separator+ databasename);
+		File database =  new File (file.getAbsolutePath() +File.separator+ databasename);
 		database.mkdirs();
 	}
 	
 	public String get_path (String databasename){
-		File database =  new File (file.getAbsolutePath() +file.separator+ databasename);
+		File database =  new File (file.getAbsolutePath() +File.separator+ databasename);
 		return  database.getAbsolutePath();
 	}
+	
+	public void deleteDatabase(String databaseName){
+    	File dir = new File(file.getAbsolutePath() + File.separator + databaseName);
+    	delete(dir);
+    }
+	
+	private void delete(File dir) {
+    	File[] files = dir.listFiles();
+        if(files!=null) {
+            for(File f: files) {
+                if(f.isDirectory()) {
+                    delete(f);
+                } else {
+                    f.delete();
+                }
+            }
+        }
+        dir.delete();
+	}
+	
+	public boolean dataBaseExists(String databaseName) {
+        for(File dir : file.listFiles()){
+            if(dir.getName().equalsIgnoreCase(databaseName))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
